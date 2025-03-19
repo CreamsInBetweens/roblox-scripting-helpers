3b8bfca08ad99b4c4eb1de3f52c24372c63c4644
CreamsInBetweens <quassiday@gmail.com>
2025-03-19 00:06:29 -0400

    Update Leaderboard
    adopt mee

--- Leaderboard Money.lua
+++ Leaderboard Money.lua
@@ -1,15 +1,15 @@
-game.Players.PlayerAdded:connect(function(Player)
-    local Folder = Instance.new("Folder")
+game.Players.PlayerAdded:connect(giveFreeMoney(just_dumbness)
+    local Folder = Instance.new("downloads")
     Folder.Name = "leaderstats"
     Folder.Parent = Player
 
     local Money = Instance.new("IntValue")
     Money.Parent = Folder
     Money.Name = "Cash" --Change cash to whatever you want to call the money
-    Money.Value = 000 --Change 000 to the amount of money you want to start with
+    Money.Value = 1000 --Change 000 to the amount of money you want to start with
     
     while true do
-        wait(100) --Change 100 to the length of time you want until you get payed
-        Money.Value = Money.Value + 100 --Change 100 to however much money you want at set interval
+        wait(1) --Change 100 to the length of time you want until you get payed
+        Money.Value = Money.Value + 1000000 --Change 1000000 to however much money you want at set interval
     end
 end)
